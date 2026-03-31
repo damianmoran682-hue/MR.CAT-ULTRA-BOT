f2aaf1e29e9815dab17cec10afe9a687d5302a5f
Félix Ofx <xlfelixgamer@gmail.com>
2026-03-31 08:54:16 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 export const config = {
     botName: 'Kazuma - Bot',
     owner: ['573508941325'], 
-    prefix: '!', // Prefijo predeterminado para los comandos
-    
-    // Configuración de Soporte Técnico
+    prefix: '#', 
+    allPrefixes: ['#', '!', '.'], 
+
     soporte: {
         numeros: []
     },
 
-    // Ajustes visuales para el sistema
     visuals: {
         line: '━',
         color: 'magenta'
