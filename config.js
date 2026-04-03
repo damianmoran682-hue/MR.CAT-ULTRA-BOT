35c6d03c68117051399dc1f68844ec51100e5ca7
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-03 18:00:01 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,36 +1,40 @@
 export const config = {
     botName: 'Kazuma - Bot',
-    // Tus dos identidades oficiales para reconocimiento total
     owner: [
         '573508941325@s.whatsapp.net', 
         '125860308893859@lid'
     ], 
-    prefix: '#', // Prefijo visual predeterminado
-    allPrefixes: ['#', '!', '.'], // Prefijos que el bot siempre reconocerá
+    prefix: '#',
+    allPrefixes: ['#', '!', '.'],
 
-    // Configuración de Soporte Técnico
     soporte: {
         numeros: []
     },
 
-    // Ajustes visuales para el sistema
     visuals: {
         line: '━',
         color: 'magenta',
-        // NUEVAS ADICIONES VISUALES
         emoji: '📥',
         emoji2: '🔍',
-        img1: 'https://link-de-tu-imagen-1.jpg', // Miniatura avisos
-        img2: 'https://link-de-tu-imagen-2.jpg', // Búsqueda grande
+        emoji3: '✿',
+        emoji4: '❀',
+        emoji5: '⛁',
+        emoji6: '❒',
+        emoji7: '⸙',
+        emoji8: '⚥',
+        emoji9: '♡',
+        emoji10: '☆',
+        emoji11: '♛',
+        img1: 'https://link-de-tu-imagen-1.jpg',
+        img2: 'https://link-de-tu-imagen-2.jpg',
         img3: 'https://link-de-tu-imagen-3.jpg'
     },
 
-    // NUEVOS MENSAJES PARA EL MENÚ
     bienvenidas: {
         bienvenida1: '¡Hola! Soy Kazuma Bot, un placer atenderte.',
         bienvenida2: 'Aquí tienes la lista de comandos:'
     },
 
-    // NUEVAS CREDENCIALES
-    apiYT: 'NEX-0868C926ADF94B19A51E18C4'
+    apiYT: 'NEX-0868C926ADF94B19A51E18C4',
+    stellarKey: 'api-Bb1JX'
 };
