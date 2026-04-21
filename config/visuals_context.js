4d596516aa3ac57adb97834c8a221b26440d1891
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 04:16:52 -0400

    Crear visuals_context.js

---
+++ config/visuals_context.js
@@ -0,0 +1,7 @@
+export const contextConfig = {
+    title: 'KAZUMA - ECONOMY SYSTEM',
+    body: 'Desarrollado por PixelCrew-Team',
+    image: 'https://files.catbox.moe/9ssbf9.jpg', // Tu imagen principal
+    link: 'https://www.instagram.com/dev.felixofc',
+    canal: 'https://whatsapp.com/channel/0029VajV9sCDzgTBr89Xv001'
+};
