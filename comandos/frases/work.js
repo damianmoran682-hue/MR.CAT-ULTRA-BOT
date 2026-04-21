449ceab88a6006be6f10d5754d1e3b1b665cae27
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 10:55:58 -0400

    Actualizar work.js

--- comandos/frases/work.js
+++ comandos/frases/work.js
@@ -1,7 +1,7 @@
 export const workFrases = [
     "Limpiaste los servidores de PixelCrew y quedaron brillantes.",
     "Programaste un bot de WhatsApp para una empresa local.",
-    "Ayudaste a Félix con el mantenimiento de Yotsudactyl.",
+    "Ayudaste a Félix con el mantenimiento de Kazuma Bot.",
     "Vendiste una base de bot bien optimizada.",
     "Fuiste moderador de un grupo de programadores por un día.",
     "Solucionaste un error crítico en el sistema de base de datos.",
