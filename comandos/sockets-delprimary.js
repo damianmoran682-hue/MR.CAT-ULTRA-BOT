f5956929390545d10ec20b7d50b720647d9a9e77
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 10:11:33 -0400

    Actualizar sockets-delprimary.js

--- comandos/sockets-delprimary.js
+++ comandos/sockets-delprimary.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import { config } from '../config.js';
 
 const jsonDir = path.resolve('./jsons');
 const databasePath = path.join(jsonDir, 'preferencias.json');
@@ -14,11 +15,11 @@ const delPrimary = {
     isGroup: true,
 
     run: async (conn, m) => {
-        const from = m.key.remoteJid;
+        const from = m.chat;
 
         if (!fs.existsSync(databasePath)) {
             return await conn.sendMessage(from, { 
-                text: `*❁* \`Sin configuración\` *❁*\n\nNo hay ningún bot primario asignado en este grupo actualmente.\n\n> ¡Todos los bots tienen permiso de respuesta!` 
+                text: `*${config.visuals.emoji2}* \`Sin configuración\`\n\nNo hay ningún bot primario asignado en este grupo actualmente.\n\n> ¡Todos los bots tienen permiso de respuesta!` 
             }, { quoted: m });
         }
 
@@ -26,16 +27,15 @@ const delPrimary = {
 
         if (!db[from]) {
             return await conn.sendMessage(from, { 
-                text: `*❁* \`Aviso\` *❁*\n\nEste grupo no tiene un bot primario fijo. Todos los sockets están activos.\n\n> ¡No hay nada que remover!` 
+                text: `*${config.visuals.emoji2}* \`Aviso\`\n\nEste grupo no tiene un bot primario fijo. Todos los sockets están activos.\n\n> ¡No hay nada que remover!` 
             }, { quoted: m });
         }
 
-        // Eliminamos la restricción del grupo
         delete db[from];
         fs.writeFileSync(databasePath, JSON.stringify(db, null, 2));
 
         await conn.sendMessage(from, { 
-            text: `*✿︎* \`Restricción Removida\` *✿︎*\n\nSe ha eliminado el bot primario con éxito. Ahora todos los sockets responderán a los comandos.\n\n> ¡La libertad ha vuelto al grupo!` 
+            text: `*${config.visuals.emoji3}* \`RESTRICCIÓN REMOVIDA\`\n\nSe ha eliminado el bot primario con éxito. Ahora todos los sockets responderán.\n\n> ¡La libertad ha vuelto al grupo!` 
         }, { quoted: m });
     }
 };
